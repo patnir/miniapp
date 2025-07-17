51ce9ba8e7eba7e574490a6ea35df89b3d5d1286
patnir <rahul.patni@coinbase.com>
2025-07-17 14:21:50 -0700

    remove mini app connector

--- app/config.ts
+++ app/config.ts
@@ -1,6 +1,6 @@
 import { http, createConfig } from 'wagmi'
 import { base } from 'wagmi/chains'
-import { farcasterMiniApp as miniAppConnector } from '@farcaster/miniapp-wagmi-connector'
+// import { farcasterMiniApp as miniAppConnector } from '@farcaster/miniapp-wagmi-connector'
 
 export const config = createConfig({
   chains: [base],
@@ -8,6 +8,6 @@ export const config = createConfig({
     [base.id]: http(),
   },
   connectors: [
-    miniAppConnector()
+    // miniAppConnector()
   ]
 })
